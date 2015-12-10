8b53b73abfe4b1001bf8565b19d09a4882a93c99
MSF Jarvis <msfjarvis@gmail.com>
2015-12-10 23:14:41 +0530

    Fixed some issues Pretty much rewrote the sucker

--- star.py
+++ star.py
@@ -1,15 +1,14 @@
-trrows = int(raw_input("Enter number of rows..."))
+trrows=int(raw_input("Enter number of rows:"))
 tr = trrows/2 + 1
-row = 0
 for row in range(1,tr+1):
-    lnospace=tr - row
+    lnospace = tr - row
     if row == 1:
-        print " " * (lnospace+1), "*"
+        print " " * (lnospace+1) , "X"
     else:
-        print " " * (lnospace),"*"," " * ((2*row - 1) - 2),"*"
+        print " " * (lnospace), "X", " " * ((2*row-1)-2), "X"
 for i in range(row,0,-1):
     if i == 1:
-        print " " * (lnospace + 1), "*"
+        print " " * (lnospace+1), "X"
     else:
-        print " " * lnospace,"*"," " * ((2*i-1)-2),"*"
-        lnospace += 1
+        print " " * (lnospace), "X", " " * ((2*i-1)-2), "X"
+        lnospace+=1
